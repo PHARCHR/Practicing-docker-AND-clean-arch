321c62079bbd4bfdfd16e2617db159af6b1f3597
PHARCHR <fargeta92@gmail.com>
2024-11-02 21:36:05 +0300

    "upadted jobsConstrollers"

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 require("express-async-errors");
-const authentication=require("./middleWares/authentication")
+const authentication = require("./middleWares/authentication");
 const connect = require("./db/connectdb");
 const authRouter = require("./routes/authRoutes");
 const jobRouter = require("./routes/jobsRoutes");
@@ -13,7 +13,7 @@ const port = 3000;
 app.use(express.json());
 //routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs",authentication,jobRouter);
+app.use("/api/v1/jobs", authentication, jobRouter);
 
 //error handling middlewares
 app.use(notFoundMiddleware);
--- controllers/jobsControllers.js
+++ controllers/jobsControllers.js
@@ -1,22 +1,26 @@
-const getAllJobs=async(req,res)=>{
-    res.send("THESE ARE ALL THE JOBS")
-}
-const getJob=async(req,res)=>{
-    res.send("SINGLE JOB")
-}
-const createJob=async(req,res)=>{
-    res.send("JOB CREATED ")
-}
-const updateJob=async(req,res)=>{
-    res.send("JOB UPDATED")
-}
-const deleteJob=async(req,res)=>{
-    res.send("JOB DELETED")
-}
-module.exports={
-    getAllJobs,
-    getJob,
-    createJob,
-    deleteJob,
-    updateJob
-}
+const Job = require("../models/jobs");
+const { StatusCodes } = require("http-status-codes");
+const getAllJobs = async (req, res) => {
+  res.send("THESE ARE ALL THE JOBS");
+};
+const getJob = async (req, res) => {
+  res.send("SINGLE JOB");
+};
+const createJob = async (req, res) => {
+  req.body.createdBy = req.user.id;
+  const job = await Job.create(req.body);
+  res.staus(StatusCodes.CREATED).json(job);
+};
+const updateJob = async (req, res) => {
+  res.send("JOB UPDATED");
+};
+const deleteJob = async (req, res) => {
+  res.send("JOB DELETED");
+};
+module.exports = {
+  getAllJobs,
+  getJob,
+  createJob,
+  deleteJob,
+  updateJob,
+};
